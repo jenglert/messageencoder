c471bf91a734afa0f0846d407f1cb423b2728c3b
Jim Englert <jimenglert@jim-englerts-macbook.local>
2009-12-15 23:21:51 -0500

    Updates.

---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+
+
+
+/**
+* Functionality that should occur when the page loads
+*/
+jQuery(document).ready( function() {
+	
+	// Supports the text area resizing capabilities.
+	jQuery('textarea.resizable:not(.processed)').TextAreaResizer();
+});
